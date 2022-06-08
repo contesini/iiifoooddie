2d38deb1bef322340d9e2ce1bebabb8169822dec
Gustavo Inacio <incio.gusta@gmail.com>
2022-06-08 14:15:35 -0300

    fix test and bump version

--- test/ifood/modules/auth/ifood-client-auth.spec.ts
+++ test/ifood/modules/auth/ifood-client-auth.spec.ts
@@ -1,47 +1,44 @@
 import axios from "axios";
 import IfoodClientAuth from "../../../../src/ifood/modules/auth";
-import * as path from 'path'
-import * as dotenv from 'dotenv'
+import * as path from "path";
+import * as dotenv from "dotenv";
 
-dotenv.config({ path: path.join(__dirname, '../.env') })
+dotenv.config({ path: path.join(__dirname, "../.env") });
 
 jest.mock("axios");
 
 describe("ifood-client-auth", () => {
-    it("should authenticate and generate token", async () => {
-      // given
-      const token = {
-        status: 200,
-        data: "xpto-token"
-      };
+  it("should authenticate and generate token", async () => {
+    // given
+    const token = {
+      status: 200,
+      data: {
+        accessToken: "xpto-token",
+      },
+    };
 
-      (axios as unknown as jest.Mock).mockResolvedValueOnce(token);
+    (axios as unknown as jest.Mock).mockResolvedValue(token);
 
-      // when
-      const result = await IfoodClientAuth.authenticate()
+    // when
+    const result = await IfoodClientAuth.authenticate();
+    expect(result).toEqual(token.data.accessToken);
+  });
+  it("should return error when credentials is invalid", async () => {
+    // given
+    const token = {
+      status: 403,
+      data: undefined,
+    };
 
-      expect(result).toEqual(token.data);
+    (axios as unknown as jest.Mock).mockResolvedValueOnce(token);
 
-
-    });
-    it("should return error when credentials is invalid", async() => {
-      // given
-      const token = {
-        status: 403,
-        data: undefined
-      };
-
-      (axios as unknown as jest.Mock).mockResolvedValueOnce(token);
-
-      try {
-        await IfoodClientAuth.authenticate()
-        
-      } catch (error) {
-        
-        expect(error.message).toBe("Please check IFOOD_CLIENT_ID and IFOOD_CLIENT_SECRET are valid")
-      }
-      // when
-
-    });
-
+    try {
+      await IfoodClientAuth.authenticate();
+    } catch (error) {
+      expect(error.message).toBe(
+        "Please check IFOOD_CLIENT_ID and IFOOD_CLIENT_SECRET are valid"
+      );
+    }
+    // when
+  });
 });
